a59195d0e8d056fb321dbd56fdeb25d8fd9368c1
Avadhesh kumar singh <63304698+Ava68@users.noreply.github.com>
2022-07-19 16:52:58 +0530

    Create maxSubSum.java

---
+++ Arrays/maxSubSum.java
@@ -0,0 +1,25 @@
+https://leetcode.com/problems/maximum-subarray/
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int maxsum = Integer.MIN_VALUE;
+        int currsum = 0;
+        
+        for(int i =0; i<nums.length; i++)
+        {
+            currsum+=nums[i];
+            
+            
+            if(currsum > maxsum)
+                maxsum = currsum;
+            
+            if(currsum < 0)
+                currsum = 0;
+                
+           
+        }
+        
+        return maxsum;
+    }
+}

64ddf83bff9190d14591abf579f38d070dd51b49
Avadhesh kumar singh <63304698+Ava68@users.noreply.github.com>
2022-07-22 23:43:07 +0530

    Create TwoSum.java

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,26 @@
+https://leetcode.com/problems/two-sum/submissions/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+      int res [] = new int[2];
+        Map<Integer,Integer>map = new HashMap<Integer,Integer>();
+        
+        for(int i =0; i<nums.length; i++)
+        {
+            if(map.containsKey(target - nums[i]))
+            {
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
+                break;
+            }
+            
+            else
+            {
+                map.put(nums[i],i);
+            }
+        }
+        
+        return res;
+    }
+}

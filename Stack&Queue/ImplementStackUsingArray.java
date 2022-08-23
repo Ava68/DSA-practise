c53a90cf009a08ad8013ae3c00b5cdbac6882a3f
Avadhesh kumar singh <63304698+Ava68@users.noreply.github.com>
2022-08-23 11:57:06 +0530

    Create ImplementStackUsingArray.java

---
+++ Stack&Queue/ImplementStackUsingArray.java
@@ -0,0 +1,38 @@
+class MyStack
+{
+    int top;
+	int arr[] = new int[1000];
+
+    MyStack()
+	{
+		top = -1;
+	}
+	
+	//Function to push an integer into the stack.
+    void push(int a)
+	{
+	    // Your code here
+	    
+	    if(top == 10000-1)
+	        return ;
+	   top++;
+	   
+	   arr[top] = a;
+	    
+	} 
+	
+    //Function to remove an item from top of the stack.
+	int pop()
+	{
+        // Your code here
+        if(top == -1)
+            return -1;
+            
+        int res = arr[top];
+        top--;
+        
+        return res;
+        
+         
+	}
+}

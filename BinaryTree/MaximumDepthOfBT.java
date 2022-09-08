15eacfddcf89b9a42820f5bca4fcd1d69bbc736b
Avadhesh kumar singh <63304698+Ava68@users.noreply.github.com>
2022-09-09 00:03:10 +0530

    Create MaximumDepthOfBT.java

---
+++ BinaryTree/MaximumDepthOfBT.java
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/maximum-depth-of-binary-tree/submissions/
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        
+        if(root == null)
+            return 0;
+        
+        int leftMax = maxDepth(root.left);
+        int rightMax = maxDepth(root.right);
+        
+        return 1 + Math.max(leftMax,rightMax);
+    }
+}

70e5dd4332d8cb1031732da921ab558b778cb5b5
Avadhesh kumar singh <63304698+Ava68@users.noreply.github.com>
2022-10-19 01:55:14 +0530

    Update UniquePaths.java

--- DynamicProgramming/UniquePaths.java
+++ DynamicProgramming/UniquePaths.java
@@ -3,7 +3,7 @@ Memoization
 class Solution {
     public int uniquePaths(int m, int n) {
         
-        int dp[][] = new int[m+1][n+1];
+        int dp[][] = new int[m][n];
         
         for(int i =0; i<m; i++)
         {

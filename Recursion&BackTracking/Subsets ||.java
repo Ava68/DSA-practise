df111b026eca70efb85ef0c3267b5c06c405159f
Avadhesh kumar singh <63304698+Ava68@users.noreply.github.com>
2024-03-03 18:00:57 +0530

    Update Subsets ||.java

--- Recursion&BackTracking/Subsets ||.java
+++ Recursion&BackTracking/Subsets ||.java
@@ -1,35 +1,34 @@
-https://leetcode.com/problems/subsets-ii/
-
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        /* sort is importnat because suppose we have a test case which is not sorted
+        [4,1,4]
+        it can generate subset like 414,41,4,14
+        this why we need to sort the array to ensures that duplicate elements are adjacent to each other. 
+        */
         Arrays.sort(nums);
-        
-        List<List<Integer>> res = new ArrayList<>();
-        
-        subsetHelper(0,nums,res,new ArrayList<Integer>());
-        
-        return res;
-    }
-    
-    public void subsetHelper(int ind,int [] nums,List<List<Integer>> res,List<Integer>sub)
-    {
-    
-        
-        res.add(new ArrayList<>(sub));
-        
-        if(ind >nums.length)
-            return;
-        
-        
-        for(int i =ind; i<nums.length; i++)
+        HashSet<List<Integer>> set = new HashSet<>();
+        helper(nums,set,new ArrayList<Integer>(),0);
+        for(List<Integer> tmp : set)
         {
-            if(i!=ind && nums[i] == nums[i-1])
-                continue;
-            
-            sub.add(nums[i]);
-            subsetHelper(i+1,nums,res,sub);
-            
-            sub.remove(sub.size()-1);
+            ans.add(new ArrayList<>(tmp));
         }
+        return ans;
+    }
+    public void helper(int [] nums,HashSet<List<Integer>> set,ArrayList<Integer> tmp,int indx)
+    {
+        if(indx >= nums.length)
+        {
+            set.add(new ArrayList<>(tmp));
+            indx--;
+            return;
+        }
+        //take
+        tmp.add(nums[indx]);
+        helper(nums,set,tmp,indx+1);
+        tmp.remove(tmp.size()-1);
+        // not take
+        helper(nums,set,tmp,indx+1);
+
     }
 }
